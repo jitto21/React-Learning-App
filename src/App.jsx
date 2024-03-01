fb95cbd58ee333ad303a1e387c1fed2692dee532
jitto21 <jittotp@gmail.com>
2024-03-01 13:51:51 +0530

    Seperate components. Sharing and outputting data between child components. Handling user events. Implement 'use state' react hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,61 @@
-import ReactImg from './assets/react-core-concepts.png'
+import { useState } from 'react';
 
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const desc = reactDescriptions[genRandomInt(1)]
-  return (
-    <header>
-      <img src={ReactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {desc} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
+import Header from './components/Header/Header';
+import CoreConcepts from './components/CoreConcepts';
+import TabButton from './components/TabButton';
+import { CORE_CONCEPTS, EXAMPLES } from './data';
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState();
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+    console.log(selectedButton, ' clicked');
+  }
+  const tabContent = !selectedTopic
+    ? <p>Please select a topic.</p>
+    : <div id="tab-content">
+      <h3>{EXAMPLES[selectedTopic].title}</h3>
+      <p>{EXAMPLES[selectedTopic].description}</p>
+      <pre>
+        <code>{EXAMPLES[selectedTopic].code}</code>
+      </pre>
+    </div>
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            {CORE_CONCEPTS.map((conceptItem, index) => {
+              return <CoreConcepts key={index} {...conceptItem} />
+            })}
+          </ul>
+        </section>
+        <section id="examples">
+          <menu>
+            <TabButton
+              onSelect={() => handleSelect('components')}
+              isActive={selectedTopic === 'components'}
+            >Component</TabButton>
+
+            <TabButton
+              onSelect={() => handleSelect('jsx')}
+              isActive={selectedTopic === 'jsx'}
+            >JSX</TabButton>
+
+            <TabButton
+              onSelect={() => handleSelect('props')}
+              isActive={selectedTopic === 'props'}
+            >Props</TabButton>
+
+            <TabButton
+              onSelect={() => handleSelect('state')}
+              isActive={selectedTopic === 'state'}
+            >State</TabButton>
+          </menu>
+          {tabContent}
+        </section>
       </main>
     </div>
   );
---
+++ src/components/TabButton.jsx
@@ -0,0 +1,4 @@
+export default function TabButton({ children, onSelect, isActive }) {
+
+   return <li><button className={isActive ? 'active' : null} onClick={onSelect}>{children}</button></li>;
+}
